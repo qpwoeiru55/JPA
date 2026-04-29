5ce9e1d7940e0c18c50d6dae35ed7b5bba7bdc7a
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2026-04-29 23:33:39 +0900

    값 타입

--- 2/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ 2/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -12,12 +12,41 @@ public class Delivery  extends BaseEntity {
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
-    private String city;
-
-    private String street;
-
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
--- 2/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ 2/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -15,11 +15,8 @@ public class Member  extends BaseEntity {
 
     private String name;
 
-    private String city;
-
-    private String street;
-
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
@@ -43,27 +40,4 @@ public class Member  extends BaseEntity {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
 }
