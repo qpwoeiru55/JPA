9e020b35905cab6d89153da92c5bbad42b6a1856
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2026-05-05 14:11:48 +0900

    jpql

---
+++ 3/jpql-master/src/main/java/jpql/Main.java
@@ -0,0 +1,93 @@
+package jpql;
+
+import org.h2.command.dml.Update;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence. createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+//            Member member = new Member();
+//            member.setUsername("jinseok");
+//            member.setAge(10);
+//            em.persist(member);
+//
+//            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+//                    .getResultList();
+//            ==============================================================
+
+            Team teamA = new Team();
+            teamA.setName("teamaA");
+            em.persist(teamA);
+
+            Team teamB  = new Team();
+            teamB.setName("teamaB");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setAge(10);
+            member1.changeTeam(teamA);
+            member1.setType(MemberType.ADMIN);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setAge(10);
+            member2.changeTeam(teamA);
+            member2.setType(MemberType.ADMIN);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setAge(10);
+            member3.changeTeam(teamB);
+            member3.setType(MemberType.ADMIN);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+//            String query = "select m from Member m inner join m.team t";
+//            List<Member> result = em.createQuery(query, Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+///////////////////////////////////////////////
+//            String query = "select m.username, 'Hello', TRUE from Member m where m.type = :usertype";
+//            List<Object[]> result = em.createQuery(query)
+//                    .setParameter("usertype",MemberType.ADMIN)
+//                    .getResultList();
+//            System.out.println(result);
+///////////////////////////////////////////////
+//            String query = "select m from Member m join fetch m.team";
+//            List<Member> result = em.createQuery(query, Member.class)
+//                    .getResultList();
+//
+//            for(Member m : result){
+//                System.out.println(m.getUsername() + m.getTeam().getName());
+//            }
+///////////////////////////////////////////////
+            int rusultCnt = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+            System.out.println(rusultCnt);
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ 3/jpql-master/src/main/java/jpql/Member.java
@@ -0,0 +1,67 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "MEMBER_ID")
+    private long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    private MemberType type;
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public MemberType getType() {
+        return type;
+    }
+
+    public void setType(MemberType type) {
+        this.type = type;
+    }
+}
